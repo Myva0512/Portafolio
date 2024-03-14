84df6da1f7b0acc9fd61aae46692c9f6eacd7568
Omega <101607443+Myva0512@users.noreply.github.com>
2024-03-14 13:20:42 -0500

    Completo

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ document.getElementById ("contacto").addEventListener("submit", function(){
     var nombre = document.querySelector("#nombre").value;
     var correo = document.querySelector("#email").value;       
     var asunto = document.querySelector("#mensaje").value; 
-    alert (nombre + "\n" + correo + "\n" + asunto )
+    if (nombre === '' || email === '' || mensaje === '') {
+        alert('Por favor, complete todos los campos.');
+    }
+    else{
+        alert ("nombre: "+ nombre + "\n" +  "correo: " + correo + "\n" + "asunto: "+ asunto )
+    }
 });  
